d6a50aa570bdfefa406ce6501276bbb54b350601
javrobs <javieroblesamar@gmail.com>
2023-09-02 20:54:38 +0800

    corrected json file

--- static/js/dropDownMenu.js
+++ static/js/dropDownMenu.js
@@ -1,22 +1,24 @@
-const URL = "http://127.0.0.1:5000/populations_all";
+const URL = "https://raw.githubusercontent.com/javrobs/nyc_lep_speakers_demo/main/static/data/records.json";
 
 function setUp(){
-    d3.json(URL).then((data)=>{
-        // console.log(data);
+    fetch(URL).then(data=>data.json()).then(data=>{
+        console.log(data);
+        data.forEach(row=>
+            console.log(row));
         // fetch languages for dropdown menu
-        let languages=[]
-        data.forEach(row =>  {
-            if (!languages.includes(row.Language)){
-            languages.push(row.Language);
-            }
-        });
-        languages.sort()
-        // console.log(languages);
-        d3.select('select').append('option').attr("value", "All").text("All");
-        languages.forEach(row => {
-            d3.select('select').append('option').attr("value" , row).text(row);
-        });
-        optionChanged("All",true,false);
+        // let languages=[]
+        // data.forEach(row =>  {
+        //     if (!languages.includes(row.Language)){
+        //     languages.push(row.Language);
+        //     }
+        // });
+        // languages.sort()
+        // // console.log(languages);
+        // d3.select('select').append('option').attr("value", "All").text("All");
+        // languages.forEach(row => {
+        //     d3.select('select').append('option').attr("value" , row).text(row);
+        // });
+        // optionChanged("All",true,false);
     });
 }
 
